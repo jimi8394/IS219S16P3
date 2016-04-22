a196c489f6fee9d001b59e6921db77957810e750
Jimmy Patel <jimi8394@gmail.com>
2016-04-22 17:48:39 -0400

    added step 3 - modified HomeController

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -98,11 +98,9 @@ app.controller('HomeController', ['$scope', function($scope) {
 	
 	/* ADD VARIABLES FOR STEP 3 HERE */
 	
-	
-	
-	
-	
-	
++      $scope.app_title = "IMDB + Jimmy's Top 8 Movies";
++      $scope.owner = "Jimmy";
++      $scope.github = "https://github.com/jimi8394/IS219S16P3";
 	
 	
 	/* ADD FUNCTIONS FOR STEP 7 HERE */
